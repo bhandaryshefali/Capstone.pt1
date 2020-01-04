e97392a99ce0c87382f4ef3c0855e1a7a62c10ef
bhandaryshefali <42041964+bhandaryshefali@users.noreply.github.com>
2020-01-04 17:22:19 +0530

    Add files via upload

---
+++ capstone pt1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This Will be used for capstone 
+# 
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
